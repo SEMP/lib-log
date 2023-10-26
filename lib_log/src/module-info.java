d08bdb3d49d5a58df2079084a8413907f795285c
Sergio Morel <sergio.morel@pti.org.py>
2023-10-26 13:45:03 -0300

    Paso de LoggerManager a librería de utilidades.

--- lib_log/src/module-info.java
+++ lib_log/src/module-info.java
@@ -5,7 +5,6 @@ module lib_log
 {
 	exports py.com.semp.lib.log;
 	exports py.com.semp.lib.log.loggers;
-	exports py.com.semp.lib.log.utilities;
 	exports py.com.semp.lib.log.configuration;
 	exports py.com.semp.lib.log.enumerations;
 	
